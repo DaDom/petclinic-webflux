6ad1190b88c566efbf08f361685c1ee1fc2e2a9c
Dominik Picker <dominik@turian.ai>
2020-01-13 16:58:09 +0200

    Initial commit

---
+++ src/main/java/com/dominik/tutorial/spring5/petclinicwebflux/PetclinicWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.dominik.tutorial.spring5.petclinicwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dominik/tutorial/spring5/petclinicwebflux/PetclinicWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominik.tutorial.spring5.petclinicwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
